ff7bdd941ff1a012686ce326c5f5c89f1e7b3988
Prigeraldes <priscillagtrindade16@gmail.com>
2026-03-04 13:06:01 -0300

    parte2acessonegado

--- Program.cs
+++ Program.cs
@@ -7,5 +7,12 @@ string name = Console.ReadLine();
 
 Console.WriteLine("Seja bem vindo: " +name);
 Console.WriteLine("Qual a sua idade?");
-int idade = Console.Read();
-Console.WriteLine("Você tem " +idade);
+int idade;
+int.TryParse(Console.ReadLine(), out idade);
+if (idade >= 18) 
+{Console.WriteLine("Você tem " +idade +" anos de idade.");
+Console.WriteLine("acesso liberado"); }
+else
+{
+    Console.WriteLine("Você é menor que 18, ACESSO NEGADO!!");
+}
