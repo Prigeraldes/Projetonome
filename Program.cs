2f0568d4bcc03b8daf13c7eff0773cd4a559214d
Prigeraldes <priscillagtrindade16@gmail.com>
2026-03-04 11:50:03 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.AccessControl;
+
+Console.WriteLine("Qual é o seu nome?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Seja bem vindo: " +name);
+Console.WriteLine("Qual a sua idade?");
+int idade = Console.Read();
+Console.WriteLine("Você tem " +idade);
