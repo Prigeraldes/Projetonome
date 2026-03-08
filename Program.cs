0badcb265872c77c9b3a20b5ba5a6966d44306c6
Prigeraldes <priscillagtrindade16@gmail.com>
2026-03-08 10:34:06 -0300

    nomeembranco

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Security.AccessControl;
+string name = "";
 
+do {
 Console.WriteLine("Qual é o seu nome?");
 
-string name = Console.ReadLine();
+name = Console.ReadLine();
 
-while (string.IsNullOrWhiteSpace (name))
-
+if (string.IsNullOrWhiteSpace (name))
 {
 Console.WriteLine("Você digitou um valor inválido, favor digite novamente");
  name = Console.ReadLine();  
 }
+
+else if (name.Any(char.IsDigit))
+{Console.WriteLine("Não pode conter números.Digite novamente");
+}
+}
+while (string.IsNullOrWhiteSpace(name)|| name.Any(char.IsDigit));
 
 Console.WriteLine("Seja bem vindo: " +name);
 Console.WriteLine("Qual a sua idade?");
