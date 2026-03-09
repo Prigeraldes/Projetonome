caceed3535d73b6e29caa8887ec614e1e0fa1958
omagister <jrenatodias@gmail.com>
2026-03-09 18:41:56 -0300

    algumas melhorias no códig

--- Program.cs
+++ Program.cs
@@ -1,32 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Security.AccessControl;
+using System.Text.RegularExpressions;
+
 string name = "";
 
 do {
-Console.WriteLine("Qual é o seu nome?");
+    Console.WriteLine("Qual é o seu nome?");
 
-name = Console.ReadLine();
+    name = Console.ReadLine();
 
-if (string.IsNullOrWhiteSpace (name))
-{
-Console.WriteLine("Você digitou um valor inválido, favor digite novamente");
- name = Console.ReadLine();  
+    if (string.IsNullOrWhiteSpace (name))
+    {
+        Console.WriteLine("Você digitou um valor inválido, favor digite novamente");
+        name = Console.ReadLine();  
+    }
+    else if (!Regex.IsMatch(name, @"^[A-Za-z]+$"))
+        Console.WriteLine("Não pode conter números.Digite novamente");
 }
+while (string.IsNullOrWhiteSpace(name)|| !Regex.IsMatch(name, @"^[A-Za-z]+$"));
 
-else if (name.Any(char.IsDigit))
-{Console.WriteLine("Não pode conter números.Digite novamente");
-}
-}
-while (string.IsNullOrWhiteSpace(name)|| name.Any(char.IsDigit));
-
-Console.WriteLine("Seja bem vindo: " +name);
+Console.WriteLine($"Seja bem vindo: {name}");
 Console.WriteLine("Qual a sua idade?");
 int idade;
 int.TryParse(Console.ReadLine(), out idade);
-if (idade >= 18) 
-{Console.WriteLine("Você tem " +idade +" anos de idade.");
-Console.WriteLine("acesso liberado"); }
-else
+if (idade > 0 && idade >= 18) 
 {
-    Console.WriteLine("Você é menor que 18, ACESSO NEGADO!!");
+    Console.WriteLine($"Você tem {idade} anos de idade.");
+    Console.WriteLine("acesso liberado");
 }
+else
+    Console.WriteLine("Você é menor que 18, ACESSO NEGADO!!");
+
